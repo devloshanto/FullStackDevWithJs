ba1f77e8e0ec154b3ec1e03d2eed2ba8532bc341
Saiful Islam Shanto <141901360+shanto-DevX@users.noreply.github.com>
2025-01-29 10:01:38 +0600

    Complete Tailwind  Website

--- 06_Tailwind/Projects/Website/toDesktop-Yt/assets/js/script.js
+++ 06_Tailwind/Projects/Website/toDesktop-Yt/assets/js/script.js
@@ -3,44 +3,58 @@ function handleMenu() {
   navDialog.classList.toggle("hidden");
 }
 
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 36 * 4;
 
-
-const initialTranslateLTR = -48*4;
-const initialTranslateRTL = 36*4;
-
-function setupInterSectionObserver(element,isLTR,spped){
-  const interSectionCallback= (entries) =>{
+function setupInterSectionObserver(element, isLTR, spped) {
+  const interSectionCallback = (entries) => {
     const isIntersecting = entries[0].isIntersecting;
     // console.log(element,isIntersecting)
 
-    if(isIntersecting){
+    if (isIntersecting) {
       document.addEventListener("scroll", scrollHandler);
-      }else{
-        document.removeEventListener("scroll", scrollHandler);
-      }
-  }
-  const interSectionObserver = new IntersectionObserver(interSectionCallback)
-  interSectionObserver.observe(element)
+    } else {
+      document.removeEventListener("scroll", scrollHandler);
+    }
+  };
+  const interSectionObserver = new IntersectionObserver(interSectionCallback);
+  interSectionObserver.observe(element);
 
-  function scrollHandler(){
-    const translateX = (window.innerHeight - element.getBoundingClientRect().top) * spped;
-    let totalTranslate=0;
-    if(isLTR){
-      totalTranslate=translateX + initialTranslateLTR;
-    }else{
+  function scrollHandler() {
+    const translateX =
+      (window.innerHeight - element.getBoundingClientRect().top) * spped;
+    let totalTranslate = 0;
+    if (isLTR) {
+      totalTranslate = translateX + initialTranslateLTR;
+    } else {
       totalTranslate = -(translateX + initialTranslateRTL);
     }
     element.style.transform = `translateX(${totalTranslate}px)`;
-}}
+  }
+}
 
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+const line4 = document.getElementById("line4");
 
-const line1= document.getElementById("line1")
-const line2= document.getElementById("line2")
-const line3= document.getElementById("line3")
-const line4= document.getElementById("line4")
+setupInterSectionObserver(line1, true, 0.15);
+setupInterSectionObserver(line2, false, 0.15);
+setupInterSectionObserver(line3, true, 0.15);
+setupInterSectionObserver(line4, true, 0.5);
 
-setupInterSectionObserver(line1,true,0.15);
-setupInterSectionObserver(line2,false,0.15);
-setupInterSectionObserver(line3,true,0.15);
-setupInterSectionObserver(line4,true,0.50);
+//
 
+const dtElement = document.querySelectorAll("dt");
+
+dtElement.forEach((element) => {
+  element.addEventListener("click", () => {
+    const ddId = element.getAttribute("aria-controls");
+    const ddElement = document.getElementById(ddId);
+    const ddArrowIcon = element.querySelectorAll("i")[0];
+
+    ddElement.classList.toggle("hidden");
+    ddElement.classList.toggle("text-red-500");
+    ddArrowIcon.classList.toggle("-rotate-180");
+  });
+});
